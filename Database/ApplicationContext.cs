193f01c45e37d950b9f84a837566070e9eaf3bed
HilariMF <133549336+HilariMF@users.noreply.github.com>
2025-04-08 18:44:59 -0400

    Configuracion del context con fluent api

---
+++ Database/ApplicationContext.cs
@@ -0,0 +1,62 @@
+﻿using Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database
+{
+    public class ApplicationContext : DbContext
+    {
+        ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region Table
+            modelBuilder.Entity<Product>()
+                .ToTable("Products");
+
+            modelBuilder.Entity<Category>()
+                .ToTable("Category");
+
+            #endregion
+
+            #region PrimaryKey
+            modelBuilder.Entity<Product>()
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<Category>()
+                .HasKey(c => c.Id);
+
+            #endregion
+
+            #region Relationships
+            modelBuilder.Entity<Category>()
+                .HasMany<Product>(category => category.Products)
+                .WithOne(p => p.Category)
+                .HasForeignKey(p => p.CategoryId);
+            #endregion
+
+            #region Property configuration
+
+            #region Product
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Name)
+                .IsRequired();
+
+            modelBuilder.Entity<Product>()
+              .Property(p => p.Price)
+              .IsRequired();
+            #endregion
+
+            #region Category
+            modelBuilder.Entity<Category>()
+              .Property(p => p.Name)
+              .IsRequired()
+              .HasMaxLength(100);
+            #endregion
+
+            #endregion
+        }
+    }
+}
