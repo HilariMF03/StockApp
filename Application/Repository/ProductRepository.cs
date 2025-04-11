c79a04cc8a354a3ac325da1234c685b49c25fefb
HilariMF <133549336+HilariMF@users.noreply.github.com>
2025-04-11 00:20:54 -0400

    Agregando nueva capa y repositorio con metodos, Add, Update, Delete, GetAll, getById

---
+++ Application/Repository/ProductRepository.cs
@@ -0,0 +1,44 @@
+﻿using Database;
+using Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Repository
+{
+    public class ProductRepository
+    {
+        private readonly ApplicationContext _dbContext;
+
+        public ProductRepository(ApplicationContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddAsync(Product product)
+        {
+            await _dbContext.Products.AddAsync(product);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Product product)
+        {
+            _dbContext.Entry(product).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Product product)
+        {
+            _dbContext.Set<Product>().Remove(product);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<Product>> GetAllAsync()
+        {
+            return await _dbContext.Set<Product>().ToListAsync();
+        }
+
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<Product>().FindAsync();
+        }
+    }
+}
