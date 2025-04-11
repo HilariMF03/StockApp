c7035a8136d2ee0e8b95ba681dd6e4f0ff50c041
HilariMF <133549336+HilariMF@users.noreply.github.com>
2025-04-11 00:37:46 -0400

    Agregando services y viewmodels

---
+++ Application/Services/ProductService.cs
@@ -0,0 +1,47 @@
+﻿using Application.Repository;
+using Application.ViewModels;
+using Database;
+using Database.Models;
+
+namespace Application.Services
+{
+    public class ProductService
+    {
+        private readonly ProductRepository _productRepository;
+
+        public ProductService(ApplicationContext dbContext)
+        {
+            _productRepository = new(dbContext);
+        }
+
+        public async Task Add(SaveProductViewModel vm)
+        {
+            Product product = new();
+            product.Name = vm.Name;
+            product.Description = vm.Description;
+            product.Price = vm.Price;
+            product.Id = vm.Id;
+            product.CategoryId = vm.CategoryId;
+
+            await _productRepository.AddAsync(product);
+
+        }
+
+        public async Task<List<ProductViewModel>> GetAllViewModel()
+        {
+            var productList = await _productRepository.GetAllAsync();
+            return productList.Select(product => new ProductViewModel
+            {
+                Name = product.Name,
+                Description = product.Description,
+                Id = product.Id,
+                ImgUrl = product.ImagUrl,
+                Price = product.Price
+
+            }).ToList();
+
+        }
+
+
+    }
+}
---
+++ Application/ViewModels/SaveProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.ViewModels
+{
+    public class SaveProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImgUrl { get; set; }
+        public double Price { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
