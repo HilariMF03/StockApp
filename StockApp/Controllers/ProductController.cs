c143b81690b8c2c5e4b934f549e71656c58455ba
HilariMF <133549336+HilariMF@users.noreply.github.com>
2025-04-12 00:41:02 -0400

    Agregando controladir de productos

---
+++ StockApp/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Application.Services;
+using Database;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StockApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductService _productService;
+
+        public ProductController(ApplicationContext dbContext)
+        {
+            _productService = new ProductService(dbContext);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
