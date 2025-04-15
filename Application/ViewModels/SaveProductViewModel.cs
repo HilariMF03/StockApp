f2b4918f1e0bb25988552c858f53eabab0ac2e07
HilariMF <133549336+HilariMF@users.noreply.github.com>
2025-04-15 00:30:55 -0400

    Validaciones desde el viewmodel

--- Application/ViewModels/SaveProductViewModel.cs
+++ Application/ViewModels/SaveProductViewModel.cs
@@ -1,12 +1,17 @@
-﻿namespace Application.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.ViewModels
 {
     public class SaveProductViewModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Debe colocar el nombre del producto")]
         public string Name { get; set; }
         public string Description { get; set; }
         public string ImagUrl { get; set; }
+        [Required(ErrorMessage = "Debe colocar el precio del producto")]
         public double Price { get; set; }
+        [Required(ErrorMessage = "Debe colocar la categoria del producto")]
         public int CategoryId { get; set; }
     }
 }
