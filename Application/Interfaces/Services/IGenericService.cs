318886a0565d07377457c6926b7d50e189280a8b
HilariMF <133549336+HilariMF@users.noreply.github.com>
2025-05-01 14:53:38 -0400

    Agregando IGenericService  (Basico)

---
+++ Application/Interfaces/Services/IGenericService.cs
@@ -0,0 +1,13 @@
+﻿namespace StockApp.Core.Application.Interfaces.Services
+{
+    public interface IGenericService<SaveViewModel, ViewModel>
+        where SaveViewModel : class
+        where ViewModel : class //toma 2 porque el ultimo metodo no espera un save sino un viewmodel y los generic pueden resivir mas de 1tipo
+    {
+        Task Update(SaveViewModel vm);
+        Task Add(SaveViewModel vm);
+        Task Delete(int id);
+        Task<SaveViewModel> GetByIdSaveViewModel(int id);
+        Task<List<ViewModel>> GetAllViewModel();
+    }
+}
