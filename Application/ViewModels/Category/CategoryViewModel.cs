d496e059474804e701133a20c85bae90bd8d34f1
HilariMF <133549336+HilariMF@users.noreply.github.com>
2025-05-01 14:41:46 -0400

    Agregando viewmodel de categoria

---
+++ Application/ViewModels/Category/CategoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace StockApp.Core.Application.ViewModels.Product
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int ProductsQuantity { get; set; } //para saber cuantos productos tiene una categoria 
+    }
+}
---
+++ Application/ViewModels/Category/SaveCategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockApp.Core.Application.ViewModels.Product
+{
+    public class SaveCategoryViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Debe colocar el nombre de la categoria")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
