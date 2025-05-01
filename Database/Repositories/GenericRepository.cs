8c2e6a5187d5916e46504faa4d92f855a39101aa
HilariMF <133549336+HilariMF@users.noreply.github.com>
2025-05-01 13:51:38 -0400

    Agreganndo GenericRepository

---
+++ Database/Repositories/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using StockApp.Infrastructure.Persistence.Contexts;
+
+namespace Application.Repository
+{
+    public class GenericRepository<Entity> where Entity : class
+    {
+        private readonly ApplicationContext _dbContext;
+
+        public GenericRepository(ApplicationContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddAsync(Entity entity)
+        {
+            await _dbContext.Set<Entity>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Entity entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Entity entity)
+        {
+            _dbContext.Set<Entity>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<Entity>> GetAllAsync()
+        {
+            return await _dbContext.Set<Entity>().ToListAsync();
+        }
+
+        public async Task<Entity> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<Entity>().FindAsync(id);
+        }
+    }
+}
