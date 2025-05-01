ace89a2424f42cbdcdff16e1dcf9eb8724aab631
HilariMF <133549336+HilariMF@users.noreply.github.com>
2025-05-01 13:56:00 -0400

    Modificando ProductRepository para usar GenericRepository

--- Database/Repositories/ProductRepository.cs
+++ Database/Repositories/ProductRepository.cs
@@ -1,46 +1,16 @@
-﻿
-using Microsoft.EntityFrameworkCore;
-using StockApp.Core.Application.Interfaces.Repositories;
+﻿using StockApp.Core.Application.Interfaces.Repositories;
 using StockApp.Core.Domain.Entities;
 using StockApp.Infrastructure.Persistence.Contexts;
 
 namespace Application.Repository
 {
-    public class ProductRepository : IProductRepository
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
     {
         private readonly ApplicationContext _dbContext;
 
-        public ProductRepository(ApplicationContext dbContext)
+        public ProductRepository(ApplicationContext dbContext) : base(dbContext)
         {
             _dbContext = dbContext;
-        }
-
-        public async Task AddAsync(Product product)
-        {
-            await _dbContext.Products.AddAsync(product);
-            await _dbContext.SaveChangesAsync();
-        }
-
-        public async Task UpdateAsync(Product product)
-        {
-            _dbContext.Entry(product).State = EntityState.Modified;
-            await _dbContext.SaveChangesAsync();
-        }
-
-        public async Task DeleteAsync(Product product)
-        {
-            _dbContext.Set<Product>().Remove(product);
-            await _dbContext.SaveChangesAsync();
-        }
-
-        public async Task<List<Product>> GetAllAsync()
-        {
-            return await _dbContext.Set<Product>().ToListAsync();
-        }
-
-        public async Task<Product> GetByIdAsync(int id)
-        {
-            return await _dbContext.Set<Product>().FindAsync(id);
         }
     }
 }
