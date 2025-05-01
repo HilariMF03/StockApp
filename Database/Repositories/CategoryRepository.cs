bce45369b1ba3e49460b7b5dab82bde3abf1d547
HilariMF <133549336+HilariMF@users.noreply.github.com>
2025-05-01 14:19:05 -0400

    Agregando CategoryRepository

---
+++ Database/Repositories/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using StockApp.Core.Application.Interfaces.Repositories;
+using StockApp.Core.Domain.Entities;
+using StockApp.Infrastructure.Persistence.Contexts;
+
+namespace Application.Repository
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationContext _dbContext;
+
+        public CategoryRepository(ApplicationContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
